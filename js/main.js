f83a78c2c20f3b80badb86ea6086f3f7b05c2e64
Jonathan Moreno <91215724+jonathanmorenofarias@users.noreply.github.com>
2023-03-26 00:18:04 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.getElementById("home-button").onclick = function () {
+    window.location.href = "http://www.youtube.com"
+}
+
+document.getElementById("about-button").onclick = function () {
+    window.location.href = "https://www.youtube.com/shorts/v8gl6hEarPQ"
+}
+
+document.getElementById("project-button").onclick = function () {
+    window.location.href = "http://www.youtube.com"
+}
+
+document.getElementById("contact-button").onclick = function () {
+    window.location.href = "http://www.youtube.com"
+}
